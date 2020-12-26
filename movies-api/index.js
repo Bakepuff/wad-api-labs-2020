4f1f951549b0a0009e302ec0205628d8dceee60f
Tan Shi <20091610@mail.wit.ie>
2020-12-26 13:07:13 +0000

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
 
@@ -16,7 +18,9 @@ const errHandler = (err, req, res, next) => {
   }
   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
 };
-
+if (process.env.SEED_DB) {
+  loadUsers();
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
