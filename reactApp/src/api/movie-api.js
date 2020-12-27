d3fc85ca73937bc6c01ab6bb9700dd1b7018e28d
Tan Shi <20091610@mail.wit.ie>
2020-12-27 14:22:43 +0000

    Topic 11 - Auth

--- reactApp/src/api/movie-api.js
+++ reactApp/src/api/movie-api.js
@@ -1,4 +1,4 @@
-export const login = (username, password) => {
+export const login = (username, password)=>{
     return fetch('/api/users', {
         headers: {
             'Content-Type': 'application/json'
@@ -16,4 +16,4 @@ export const signup = (username, password) => {
         method: 'post',
         body: JSON.stringify({ username: username, password: password })
     }).then(res => res.json())
-};
+}; 
